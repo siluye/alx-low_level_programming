0d63556a39721ef38461c5f3da0dc89e0f43b67f
siluye <seife@yekicomp.com>
2022-09-14 11:02:00 -0700

    Task 9 and 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the results
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			prod = i * j;
-			if (j != 0) 
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
