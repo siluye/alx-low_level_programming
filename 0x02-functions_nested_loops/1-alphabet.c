9b0ba956ffd253aecda9b224fc5f5b7469dd239e
siluye <seife@yekicomp.com>
2022-09-13 19:25:47 -0700

    1-alphabet and 2-alphabet_x10

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point (alphabe print)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	_putchar(alpha);
+
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
+	char alpha;
 	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
