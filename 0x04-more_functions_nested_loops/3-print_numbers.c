537a2fe8a7d82d2f8f49a09ee37cccf7c9c48e60
siluye <seife@yekicomp.com>
2022-09-15 17:31:23 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
