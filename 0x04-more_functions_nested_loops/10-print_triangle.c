dbb6ef50ddb3fd7a94153e2fc5634c29c9591e79
siluye <seife@yekicomp.com>
2022-09-15 23:46:00 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,14 +14,14 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = i; j < size; j++)
+			for (j = size - i; j > 0; j--)
 			{
 				_putchar(' ');
 			}
 
-			for (j = 1; j <= i; j++)
+			for (j = 0; j < i; j++)
 			{
-				_putchar(35);
+				_putchar('#');
 			}
 			if (i == size)
 			{
