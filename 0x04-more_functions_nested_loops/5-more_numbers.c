a605bf3548e364e0331acc07ae167c0892a5d99c
siluye <seife@yekicomp.com>
2022-09-15 19:32:34 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,9 +16,9 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar((j / 10) + 48);
+				_putchar((j / 10) + '0');
 			}
-			_putchar((j % 10) + 48);
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n')
 	}
