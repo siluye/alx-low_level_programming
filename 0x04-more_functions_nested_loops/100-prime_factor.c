65ed95731f25cd62d327004a85b5d8d99ebf1233
siluye <seife@yekicomp.com>
2022-09-17 08:39:32 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,23 +11,16 @@ int main(void)
 	long int n, m;
 	n = 612852475143;
 
-	while (m++ < n / 2)
+	
+	for (m = 2; m <= n; m++)
 	{
 		if (n % m == 0)
 		{
-			n /= 2;
-			continue;
+			n /= m;
+			m--;
 		}
-		
-		for (m = 3; m < n / 2; m += 2)
-		{
-			if (n % m == 0)
-			{
-				n /= m;
-			}
-		}
-		printf("%ld\n", n);
-
-		return (0);
 	}
+	printf("%ld\n", n);
+
+	return (0);
 }
