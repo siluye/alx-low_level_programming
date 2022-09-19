47d04570c40a2394652e2c9097ffe134df9bd402
siluye <seife@yekicomp.com>
2022-09-19 14:09:22 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int leng = 0;
+	while (*(s + leng) != '\0')
+		leng++;
+
+	return (leng);
+}
