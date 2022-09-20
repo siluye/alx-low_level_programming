5f6888d66ce913a742958575dbff7731d56ad0d8
siluye <seife@yekicomp.com>
2022-09-20 14:49:22 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	while (*(src + i) != '\0')
+	while (i >= 0)
 	{
 		*(dest + i) = *(src + i);
+
+		if(*(src + i) == '\0')
+		{
+			break;
+		}
 		i++;
 	}
 	*(dest + i) = '\0';
