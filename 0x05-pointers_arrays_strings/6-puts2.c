b4adb9a871c9ac95d799b2eb8c5b7f0a6da5e10b
siluye <seife@yekicomp.com>
2022-09-20 13:23:49 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,15 +10,13 @@
 
 void puts2(char *str)
 {
-	int leng = 0, i = 0;
+	int i = 0;
 
-	while (str[leng] != '\0')
-		leng++;
-
-	leng -= 1;
-
-	for (; i <= len; i += 2)
-		_putchar(str[i]);
-
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
 	_putchar('\n');
 }
