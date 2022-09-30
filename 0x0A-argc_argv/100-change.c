0870e80c31815da9cd5ad7c0893340d10a0f3561
siluye <seife@yekicomp.com>
2022-09-30 16:26:27 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,43 +12,35 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int w, x, y, z;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc == 1 || argc > 2)
+	w = x = y = z = 0;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
 
-	while (cents > 0)
+	x = atoi(argv[1]);
+
+	if (x <= 0)
 	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-		}
-		else if (cents >= 1)
-		{
-			cents -= 1;
-		}
-		else
-		{
-			ncoins += 1;
-		}
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", ncoins);
+	while (coins[w] != '\0')
+	{
+		if (x >= coins[w])
+		{
+			z = (x / coins[w]);
+			y += z;
+			x -= coins[w * z;
+		}
+		w++;
+	}
+	printf("%d\n", y);
 
 	return (0);
 }
