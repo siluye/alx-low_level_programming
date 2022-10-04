0611bf760a7dbafcfd81d5fa491647e5e538ccef
siluye <seife@yekicomp.com>
2022-10-04 12:08:46 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
+	char *str;
 	int i, j = 0, leng = 0;
 
 	if (s1 == NULL)
@@ -23,16 +23,16 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		leng++;
 
-	j = malloc(sizeof(char) * leng);
+	str = malloc(sizeof(char) * leng);
 
-	if (j == NULL)
+	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
-		concat_str[j++] = s1[i];
+		str[j++] = s1[i];
 
 	for (i = 0; s2[i]; i++)
-		concat_str[j++] = s2[i];
+		str[j++] = s2[i];
 
-	return (concat_str);
+	return (str);
 }
