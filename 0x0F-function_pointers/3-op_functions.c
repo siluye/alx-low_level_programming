7b3adc09daa11d87265b488bd64ecc23ef1ec29a
siluye <seife@yekicomp.com>
2022-10-12 19:21:14 -0700

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -44,7 +44,6 @@ int op_mul(int a, int b)
  * @a: first number.
  * @b: second number.
  *
- *
  * Return: division.
  */
 
@@ -61,36 +60,21 @@ int op_div(int a, int b)
 	}
 }
 
-
-
 /**
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- *  * op_mod - calculates the module of two numbers.
- *
- *   * @a: first number.
- *
- *    * @b: second number.
- *
- *     *
- *
- *      * Return: remainder of the division.
- *
- *       */
+ * Return: remainder of the division.
+ */
 
 int op_mod(int a, int b)
-
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-		if (b == 0)
-
-				{
-
-							printf("Error\n");
-
-									exit(100);
-
-										}
-
-			return (a % b);
-
+	return (a % b);
 }
