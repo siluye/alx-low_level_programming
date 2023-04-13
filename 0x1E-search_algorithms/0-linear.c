954b346ac57be501e6cb56252bf45b8f57d2c339
siluye <seife@yekicomp.com>
2023-04-13 02:07:32 -0700

    updates

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,13 +15,17 @@ int linear_search(int *array, size_t size, int value)
 	size_t x;
 
 	if (array == NULL)
+	{
 		return (-1);
+	}
 
 	for (x = 0; x < size; x++)
 	{
 		printf("Value checked array[%li] = [%i]\n", x, array[x]);
 		if (array[x] == value)
+		{
 			return (x);
+		}
 	}
 	return (-1);
 }
